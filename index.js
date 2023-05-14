ee1f628d2f06bf6e0bba1758327026ea31801c4d
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-05-14 22:31:35 +0400

    Add new chocolate system added.

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
     res.send("Chocolate Management System Running Successfully!")
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cp5mulo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    // Create Connection
+    const chocolateConnection = client.db("chocolateDB").collection("chocolate");
+
+
+    app.post("/chocolate", async (req, res) => {
+      const newChocolate = req.body;
+      console.log(newChocolate);
+      const result = await chocolateConnection.insertOne(newChocolate);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
     console.log(`Our Chocolate Management Server is running on ${port}`);
